711c8572102d354e884da5c35d8db45a934db270
7c00 <i@7c00.cc>
2021-03-26 18:14:20 +0800

    Update

--- lib/hyphenate_chat.dart
+++ lib/hyphenate_chat.dart
@@ -1,14 +1,13 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+// ignore: avoid_classes_with_only_static_members
 class HyphenateChat {
-  static const MethodChannel _channel =
-      const MethodChannel('hyphenate_chat');
+  static const MethodChannel channel = MethodChannel('hyphenate_chat');
 
   static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+    final String version = await channel.invokeMethod('getPlatformVersion');
     return version;
   }
 }
